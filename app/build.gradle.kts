fe15c54e3001d3f3e2d531f90d72a939df0e7c62
pileOwheat <haomer246@gmail.com>
2025-04-29 14:26:02 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.csproject"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.csproject"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+
+    // OkHttp for WebSocket
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+// Firebase Realtime Database (using BoM for version management)
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-database")
+// AndroidX Fragments
+    implementation ("androidx.fragment:fragment:1.8.6")
+
+}
